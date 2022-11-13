ad17cbd3f716eadc3a59ee1b5cdd17e0d28f1b1a
LuisGonzalez27 <luis27coding@gmail.com>
2022-11-13 19:05:28 +0100

    add cards

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,13 +1,38 @@
 <template>
+    <div class="wrapper">
+        <div class="my-container">
+            <div class="row">
+                <ProductComponent v-for="(item, index) in comics" :key="index" :obj="item" />
+            </div>
+            <div class="d-flex justify-content-center">
+                <button class="btn btn-primary text-uppercase strong">loard more</button>
+            </div>
+        </div>
 
+    </div>
 </template>
 
 <script>
+import ProductComponent from './ProductComponent.vue';
+import { products } from '../data/dc-comics.js';
+
 export default {
-    name: 'MainComponent'
+    components: {
+        ProductComponent
+    },
+    data() {
+        return {
+            comics: products,
+        }
+    }
 }
 </script>
 
 <style lang="scss" scoped>
+@use '../assets/styles/partials/mixins' as *;
+@use '../assets/styles/partials/variables' as *;
 
+.wrapper {
+    padding: 3rem 0 1.5rem;
+}
 </style>
---
+++ src/components/ProductComponent.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="col">
+        <div class="img-comic">
+            <img :src="obj.thumb" :alt="obj.series">
+        </div>
+        <h4>{{ obj.series }}</h4>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ProductComponent',
+    props: [
+        'obj'
+    ]
+}
+</script>
+
+<style lang="scss" scoped>
+@use '../assets/styles/partials/variables' as *;
+@use '../assets/styles/partials/mixins' as *;
+
+.col {
+    flex-basis: calc(100% / 6);
+}
+
+.img-comic {
+    height: 170px;
+    overflow: hidden;
+}
+
+h4 {
+    color: $white;
+    font-size: 12px;
+    text-transform: uppercase;
+    padding: 1rem 0;
+}
+</style>
